661525abafc5830444e5262662b2ab4a406e8ef5
joelekman <joelekm@kth.se>
2014-06-01 00:03:32 +0200

    code for basic calculating saley is done, started with design

--- app/js/controllers/CalcSaleryCtrl.js
+++ app/js/controllers/CalcSaleryCtrl.js
@@ -1,39 +1,67 @@
-'use strict';
-
-/* global eaccountingControllers */
-/* global console */
-/* global alert */
-/* jshint globalstrict: true */
+/*global eaccountingControllers */
+/*global console */
+/*global alert */
+/*jshint globalstrict: true */
 
 
-eaccountingControllers.controller('CalcSaleryCtrl', ['$scope', 
+eaccountingControllers.controller('CalcSaleryCtrl', ['$scope',
     function ($scope) {
-        console.log("test");
-        var eFee = (1-0.1488);
-        var taxFee = 0.3048;
+        'use strict';
+        var eFee = (1 - 0.1488), taxFee = 0.3048;
 
-                       /* double tax = (salery*0.3048);
-						double eFee = (salery/0.8512)-salery;
-						double taxEfee = tax + eFee;
-						double withdraw = (salery/0.8512);
-						double saleryAfterTax = salery - tax;*/
           
-          
-        $scope.counteFromSaleryWithTax = function() {
-            console.log("i beräkna med skatt");
-            /*if ($scope.saleryWithTax !== undefined){*/
-               $scope.count(Math.round($scope.saleryWithTax/eFee));
-           /* } else {
+        $scope.counteOwnWithdrawl = function () {
+            console.log("i beräkna med ownWithdrawl");
+            if ((typeof $scope.ownWithdrawl) === 'number') {
+                $scope.count($scope.ownWithdrawl);
+            } else {
+                alert("Då måste ange ett hur stort det egna utaget ska vara");
+            }
+        };
+        
+        
+        $scope.counteSaleryWithTax = function () {
+            console.log("i beräkna med Lön med skatt");
+            if ((typeof $scope.ownWithdrawl) === 'number') {
+                $scope.count(Math.round($scope.saleryWithTax / eFee));
+            } else {
                 alert("Då måste ange ett hur mycket bruttolön du vill ha");
-            }*/
+            }
         };
+        
+        $scope.counteTax = function () {
+            console.log("i beräkna med skatt");
+            if ((typeof $scope.ownWithdrawl) === 'number') {
+                $scope.count(Math.round(($scope.tax / taxFee) / eFee));
+            } else {
+                alert("Då måste ange hur mycket skatt du vill betala");
+            }
+        };
+        
+        $scope.counteEmoloymentFee = function () {
+            console.log("i beräkna med egenavgift");
+            if ((typeof $scope.ownWithdrawl) === 'number') {
+                $scope.count(Math.round($scope.employmentFee / (1 - eFee)));
+            } else {
+                alert("Då måste ange hur mycket egenavgift du vill betala");
+            }
+        };
+                            
+        $scope.countSaleryWithOutTax = function () {
+            console.log("i beräkna med nettolön");
+            if ((typeof $scope.ownWithdrawl) === 'number') {
+                $scope.count(Math.round(($scope.saleryWithOutTax / (1 - taxFee)) / eFee));
+            } else {
+                alert("Då måste ange ett hur mycket nettolön du vill ha");
+            }
+        };
+        
     
-        $scope.count = function (ownWithdrawl){
+        $scope.count = function (ownWithdrawl) {
             $scope.ownWithdrawl = ownWithdrawl;
-            $scope.saleryWithTax = Math.round(ownWithdrawl*eFee);
-            $scope.tax = Math.round($scope.saleryWithTax*taxFee);
-            $scope.employmentFee = Math.round(ownWithdrawl*(1-eFee));
-            $scope.saleryWithOutTax = Math.round($scope.saleryWithTax*(1-taxFee));
+            $scope.saleryWithTax = Math.round(ownWithdrawl * eFee);
+            $scope.tax = Math.round($scope.saleryWithTax * taxFee);
+            $scope.employmentFee = Math.round(ownWithdrawl * (1 - eFee));
+            $scope.saleryWithOutTax = Math.round($scope.saleryWithTax * (1 - taxFee));
         };
-    }
-]);
+    }]);
